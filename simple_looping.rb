36c8b5013394fe19569419080de9db8122bffc2e
Ryan Ditzel <ryand370@gmail.com>
2019-04-28 23:55:29 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -61,7 +61,7 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
-  for counter in number_of_times do
+  for counter in 1..number_of_times do
     puts phrase
   end
 end
